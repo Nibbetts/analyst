b4e0ed4cd31b2674caa7e0066eb0cfaf1cb0dd49
nibbetts <nathan.paul.tibbetts@gmail.com>
2018-01-26 12:47:02 -0700

    Added a comment.

--- clusters/__init__.py
+++ clusters/__init__.py
@@ -2,6 +2,7 @@
 #from analyst.clusters import Cluster
 #from analyst.clusters import Node
 
-# Recursive way:
+# Recursive way: (Not necessary if parent package does it non-recursively,
+#   but makes it useable by itself):
 from Cluster import *
 from Node import *
